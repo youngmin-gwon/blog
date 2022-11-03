9fd024318d19b80c9e67b80c560eeecb146af9c0
youngmin-gwon <youngmin.gwon@gmail.com>
2022-11-03 20:23:20 +0900

    fix(usecase): remove constructor for usecase
    leave usecase contract as it is

--- lib/src/core/domain/usecase/usecase.dart
+++ lib/src/core/domain/usecase/usecase.dart
@@ -2,6 +2,5 @@ import 'package:blog/src/core/domain/entity/failure.dart';
 import 'package:dartz/dartz.dart';
 
 abstract class Usecase<Type, Params> {
-  const Usecase();
   Future<Either<Failure, Type>> call(Params params);
 }
